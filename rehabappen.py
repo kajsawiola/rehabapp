1b6707a685ddfb6852a09ecf972e3014fd561ab5
kajsawiola <kajsam@hotmail.com>
2023-05-16 13:32:53 +0200

    Importerat ttk och gjort frame för att få alla button i vågrätt linje och kunna anpassa mellanrummet mellan dem.

--- rehabappen.py
+++ rehabappen.py
@@ -1,6 +1,7 @@
 #rehabappen
 import tkinter as tk
 import webbrowser
+from tkinter import ttk
 
 def open_pdf():
     rehabProgram = r"C:\Users\Användar\PycharmProjects\pythonProject2\projketarbete maj23\Kajsa.pdf"
@@ -15,18 +16,22 @@ def open_diagnos():
 window = tk.Tk()
 
 welcome = tk.Label(window, text="Välkommen, Kajsa")
-info = tk.Label(window, text="Här har du tillgång till allt som rör ditt besök på sjukgymnastiken. Navigera genom att klicka på de olika knapparna.")
-nyTid = tk.Label(window, text="Du rekommenderas boka en ny tid efter 3 veckors rehabträning.")
 welcome.pack()
+info = tk.Label(window, text="Här har du tillgång till allt som rör ditt besök på sjukgymnastiken. Navigera genom att klicka på de olika knapparna.")
 info.pack()
+nyTid = tk.Label(window, text="Du rekommenderas boka en ny tid efter 3 veckors rehabträning.")
 nyTid.pack()
 
-rehab_button = tk.Button(window, text= "Visa träningsprogram", command = open_pdf)
-link_button = tk.Button(window, text = "Boka ny tid", command = open_boka)
-diagnos_button = tk.Button(window, text = "Information om mina besvär", command =open_diagnos )
+button_frame = ttk.Frame(window)
 
-rehab_button.pack()
-link_button.pack()
-diagnos_button.pack()
+rehab_button = ttk.Button(button_frame, text= "Visa träningsprogram", command = open_pdf)
+boka_button = ttk.Button(button_frame, text = "Boka ny tid", command = open_boka)
+diagnos_button = ttk.Button(button_frame, text = "Information om mina besvär", command =open_diagnos )
+
+boka_button.pack(side="left", padx=5, pady=5)
+rehab_button.pack(side="left", padx=5, pady=5)
+diagnos_button.pack(side="left", padx=5, pady=5)
+
+button_frame.pack()
 
 window.mainloop()
