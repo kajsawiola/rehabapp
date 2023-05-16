13f2859d03d810ceb63a8d3930816295a29d1bee
kajsawiola <kajsam@hotmail.com>
2023-05-16 11:04:49 +0200

    Imporeterat tkinter och lagt till 3 st label med textinfomation.

--- rehabappen.py
+++ rehabappen.py
@@ -1,1 +1,13 @@
 #rehabappen
+import tkinter as tk
+
+window = tk.Tk()
+
+welcome = tk.Label(window, text="Välkommen, Kajsa")
+info = tk.Label(window, text="Här har du tillgång till allt som rör ditt besök på sjukgymnastiken. Navigera genom att klicka på de olika knapparna.")
+nyTid = tk.Label(window, text="Du rekommenderas boka en ny tid efter 3 veckors rehabträning.")
+welcome.pack()
+info.pack()
+nyTid.pack()
+
+window.mainloop()
