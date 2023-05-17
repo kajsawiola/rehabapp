46f5c5332a7822c7c09e4054fb93e0ae013ef277
kajsawiola <kajsam@hotmail.com>
2023-05-17 09:11:18 +0200

    Ändrat format för att kunna illustrera en apps storlek i mobilen.

--- rehabappen.py
+++ rehabappen.py
@@ -4,7 +4,7 @@ import webbrowser
 from PIL import ImageTk, Image
 
 def open_pdf():
-    rehabProgram = r"C:\Users\Användar\PycharmProjects\pythonProject2\projketarbete maj23\Kajsa.pdf"
+    rehabProgram = r"C:\Users\Användar\PycharmProjects\pythonProject2\projketarbete maj23\Rehab.pdf"
     webbrowser.open(rehabProgram)
 
 def open_boka():
@@ -17,23 +17,23 @@ window = tk.Tk()
 window.configure(bg="#e4d5b7")
 
 welcome = tk.Label(window, text="Välkommen, Kajsa", font=("Consolas", 40, "italic", "bold"), fg="#800080", bg="#e4d5b7")
-welcome.pack(pady=50)
-info = tk.Label(window, text="Här har du tillgång till allt som rör ditt besök på sjukgymnastiken. Navigera genom att klicka på de olika knapparna.", font=("Consolas", 14), fg="#800080", bg="#e4d5b7")
-info.pack(pady=30)
+welcome.pack(pady=20)
+info = tk.Label(window, text="Här har du tillgång till allt som rör ditt besök på sjukgymnastiken.\n Navigera genom att klicka på de olika knapparna.", font=("Consolas", 14), fg="#800080", bg="#e4d5b7")
+info.pack(pady=15)
 nyTid = tk.Label(window, text="Du rekommenderas boka en ny tid efter 3 veckors rehabträning.", font=("Consolas", 14), fg="#800080", bg="#e4d5b7")
-nyTid.pack(pady= 30)
+nyTid.pack(pady= 15)
 
 button_frame = tk.Frame(window)
 button_frame.configure(bg="#e4d5b7")
 
 
-rehab_button = tk.Button(button_frame, text= "Visa träningsprogram", command = open_pdf, width=30, height=10, font=("Verdana", 16, "bold"), bg="#8c92ac", fg="#5b264f" )
-boka_button = tk.Button(button_frame, text = "Boka ny tid", command = open_boka, width=30, height=10, font=("Verdana", 16, "bold"), bg="#8c92ac", fg="#5b264f")
-diagnos_button = tk.Button(button_frame, text = "Information om mina besvär", command =open_diagnos, width=30, height=10, font=("Verdana", 16, "bold"), bg="#8c92ac", fg="#5b264f")
+rehab_button = tk.Button(button_frame, text= "Visa träningsprogram", command = open_pdf, width=25, height=5, font=("Verdana", 16, "bold"), bg="#8c92ac", fg="#5b264f" )
+boka_button = tk.Button(button_frame, text = "Boka ny tid", command = open_boka, width=25, height=5, font=("Verdana", 16, "bold"), bg="#8c92ac", fg="#5b264f")
+diagnos_button = tk.Button(button_frame, text = "Information om mina besvär", command =open_diagnos, width=25, height=5, font=("Verdana", 16, "bold"), bg="#8c92ac", fg="#5b264f")
 
-boka_button.pack(side="left", padx=50, pady=60)
-rehab_button.pack(side="left", padx=50, pady=60)
-diagnos_button.pack(side="left", padx=50, pady=60)
+boka_button.pack(side="top", padx=20, pady=20)
+rehab_button.pack(side="top", padx=20, pady=20)
+diagnos_button.pack(side="top", padx=20, pady=20)
 
 button_frame.pack()
 
@@ -45,7 +45,7 @@ photo = ImageTk.PhotoImage(image)
 image_frame = tk.Frame(window)
 image_frame.pack(pady=30)
 
-for i in range(8):
+for i in range(4):
     image_label = tk.Label(image_frame, image=photo)
     image_label.grid(row=0, column=i)
 
